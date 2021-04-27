ca068d54354a68d5a7719e6124fe811ee0176a23
David Boone <Dboone2@mail.greenriver.edu>
2021-04-27 13:48:59 -0400

    Created views direcotry and added routing

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $f3 = Base::instance();
 $f3->route('GET /', function () {
     //Display the home page
     $view = new Template();
-    echo "Pet Home";
+    echo $view->render("views/home.html");
 });
 
 //Run Fat-Free
